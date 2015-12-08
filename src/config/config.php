58df52eeaa96e57e8667b6446f35b5f7534c598e
Stephane.MARTIGNIER <stephanemartignier@gmail.com>
2015-12-08 15:51:23 +0100

    re-add example of config.php

---
+++ src/config/config.php
@@ -0,0 +1,21 @@
+<?php
+
+// MYSQL information
+//------------------
+define ('MYSQLHOST',	"IP/Name server");		// ServerName
+define ('MYSQLUSER',	"Username");			// UserName
+define ('MYSQLPWD',		"Password");				// UserPassword
+define ('DATABASE',		"name_database");		// DBName
+////////////////////////////////////////
+
+
+// TheDBMovie CONST
+//-----------------
+define ('THEDBMOVIEKEY',	"API Key TheMovieDB");		// TheMovieDB api key
+////////////////////////////////////////
+
+
+// User folder
+//-----------------
+define ('USERFOLDER',	"Path to the folder");				// User folder
+////////////////////////////////////////
